4d973e9de772e3c11cb366dbae41545ea7ebca50
Steve Ziuchkovski <zman97211@gmail.com>
2017-04-05 19:28:51 -0400

    Works, graphs the "GameReports" from the save file.

--- SimAirportStatsViewer/MainWindow.xaml.cs
+++ SimAirportStatsViewer/MainWindow.xaml.cs
@@ -5,11 +5,14 @@ using System.Linq;
 using System.Reflection;
 using System.Windows;
 using Newtonsoft.Json.Linq;
+using OxyPlot;
+using OxyPlot.Series;
 
 namespace SimAirportStatsViewer
 {
-    internal class GameReport
+    public class GameReport
     {
+        public Int64 DayNumber;
         public double s_sentiment;
         public Int64 s_i;
         public Int64 n_carousels;
@@ -29,27 +32,53 @@ namespace SimAirportStatsViewer
     /// </summary>
     public partial class MainWindow : Window
     {
-        private List<GameReport> ReadReports(string inputFilename)
+        private IEnumerable<GameReport> ReadReports(string inputFilename)
         {
             dynamic d = JObject.Parse(File.ReadAllText(inputFilename));
             var gameReports = d["GameReports"];
             var reports = new List<GameReport>();
-            foreach (var gameReportKey in gameReports)
+            foreach (var gameReport in gameReports)
             {
-                foreach (var gameReport in gameReportKey)
+                var report = new GameReport();
+                report.DayNumber = Int64.Parse(gameReport.Name);
+                foreach (var gameReportKeyValuePair in gameReport)
                 {
-                    var report = new GameReport();
-                    foreach (var nameValuePair in gameReport)
+                    foreach (var keyValuePair in gameReportKeyValuePair)
                     {
                         var fields = typeof(GameReport).GetFields(BindingFlags.Public | BindingFlags.Instance);
-                        var field = fields.FirstOrDefault(f => f.Name == nameValuePair.Name);
+                        var field = fields.FirstOrDefault(f => f.Name == keyValuePair.Name);
                         if (field != null)
-                            field.SetValue(report, nameValuePair.Value.Value);
+                            field.SetValue(report, keyValuePair.Value.Value);
                     }
-                    reports.Add(report);
                 }
+                reports.Add(report);
             }
-            return reports;
+            return reports.OrderBy(r => r.DayNumber).ToList();
+        }
+
+        public List<PlotModel> PlotModels { get; set; } = new List<PlotModel>();
+
+        private PlotModel GeneratePlot(IEnumerable<GameReport> reports, string propertyName)
+        {
+            var fields = typeof(GameReport).GetFields(BindingFlags.Public | BindingFlags.Instance);
+            var field = fields.FirstOrDefault(f => f.Name.Equals(propertyName));
+            if (field == null)
+                throw new ArgumentException();
+
+            var plot = new PlotModel();
+            var series = new LineSeries();
+
+            foreach (var report in reports)
+            {
+                var value = double.Parse(field.GetValue(report).ToString());
+                series.Points.Add(new DataPoint(report.DayNumber, value));
+            }
+
+            plot.Series.Add(series);
+
+            plot.Title = propertyName;
+
+            return plot;
         }
 
         public MainWindow()
@@ -57,7 +86,20 @@ namespace SimAirportStatsViewer
             InitializeComponent();
 
             var inputFilename = @"C:\Users\steve\Desktop\README.airport.json";
-            var reports = ReadReports(inputFilename);
+
+            PlotModels.Add(GeneratePlot(ReadReports(inputFilename), "s_sentiment"));
+            PlotModels.Add(GeneratePlot(ReadReports(inputFilename), "s_i"));
+            PlotModels.Add(GeneratePlot(ReadReports(inputFilename), "n_carousels"));
+            PlotModels.Add(GeneratePlot(ReadReports(inputFilename), "ScheduledPassengers_Departures"));
+            PlotModels.Add(GeneratePlot(ReadReports(inputFilename), "MissedFlight"));
+            PlotModels.Add(GeneratePlot(ReadReports(inputFilename), "BoardedFlight"));
+            PlotModels.Add(GeneratePlot(ReadReports(inputFilename), "LuggageLost"));
+            PlotModels.Add(GeneratePlot(ReadReports(inputFilename), "LuggageSucceeded"));
+            PlotModels.Add(GeneratePlot(ReadReports(inputFilename), "FlightsCount"));
+            PlotModels.Add(GeneratePlot(ReadReports(inputFilename), "FlightsCanceled"));
+            PlotModels.Add(GeneratePlot(ReadReports(inputFilename), "FlightsDelayed"));
+
+            DataContext = this;
         }
     }
 }
